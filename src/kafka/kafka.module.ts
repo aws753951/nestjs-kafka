5478cb67c517b8a7a7ee576e46d0712cf4612794
Marc zhu <a0983204766@gmail.com>
2024-04-16 23:27:56 +0800

    kafka.module: 加入producer & consumer service

--- src/kafka/kafka.module.ts
+++ src/kafka/kafka.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ProducerService } from './producer.service';
+import { ConsumerService } from './consumer.service';
 
-@Module({})
+@Module({
+  providers: [ProducerService, ConsumerService],
+  exports: [ProducerService, ConsumerService],
+})
 export class KafkaModule {}
